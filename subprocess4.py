9342c564a4328bc4205136fb8a66800202d0da69
David Gahima <dgahima3@gmail.com>
2026-09-24 17:47:48 +0300

    Update nmap command in subprocess4.py
    Removed the '-sn' option from the nmap command.

--- subprocess4.py
+++ subprocess4.py
@@ -4,6 +4,6 @@ targets = ["10.1.0.1" , "192.168.1.1" , "172.16.81.1"]
 
 for target in targets:
 
-    output = subprocess.run(["nmap" , "-sn" , "-sV" , "-sC" , target] , capture_output = True , text = True)
+    output = subprocess.run(["nmap" ,"-sV" , "-sC" , target] , capture_output = True , text = True)
 
     print(output.stdout)
